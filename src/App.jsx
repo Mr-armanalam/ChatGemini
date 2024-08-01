2f126268b082c68197ff19303ba114f1d7a59179
Arman Alam <armanalam78578@gmail.com>
2024-08-01 11:39:49 +0530

    all set

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "module";
+import Sidebar from "./comoponents/Sidebar/Sidebar";
+import Main from "./comoponents/Main/Main";
+
+function App() {
+
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
+
+{/* <></> :-fragment */}
